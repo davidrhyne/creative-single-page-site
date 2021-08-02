2c0076c6e2a3913d08d3de341f7cae0eb8e22f88
DAVID J RHYNE <davidrhyne@att.net>
2021-08-02 11:20:23 -0500

    completing first pass a mobile-first styling

--- dist/src/creative.js
+++ dist/src/creative.js
@@ -8,6 +8,11 @@ const menuItems = document.querySelectorAll('.menu-item')
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
+const slides = document.getElementsByClassName('project__carousel-item')
+const slidesTitles = document.getElementsByClassName('product__carousel-item-title')
+let slidePosition = 0
+const totalSlides = slides.length
+
 let isMenuOpen = false;
 
 function handleMenuClick() {
@@ -40,3 +45,59 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+// carousel slides + associated slide titles
+document.getElementById('project__carousel-nav-button-next').addEventListener('click', moveToNextSlide);
+document.getElementById('project__carousel-nav-button-previous').addEventListener('click', moveToPrevSlide);
+
+console.log('slidePosition = ', slidePosition)
+console.log('totalSlides = ', totalSlides)
+console.log('slidesTitles.length = ', slidesTitles.length)
+
+function hideAllSlides() {
+    for (let slide of slides) {
+        //product__carousel-item-title--visible
+        //product__carousel-item-title--hidden
+        slide.classList.remove('project__carousel-item--visible');
+        slide.classList.add('project__carousel-item--hidden');
+
+        //slidesTitles.classList.remove('product__carousel-item-title--visible');
+        //slidesTitles.classList.add('product__carousel-item-title--hidden');
+    }
+
+    for (let title of slidesTitles) {
+        title.classList.remove('product__carousel-item-title--visible');
+        title.classList.add('product__carousel-item-title--hidden');
+    }
+}
+
+function moveToNextSlide() {
+    console.log('next button has been clicked')
+    //console.log('slidePosition before = ', slidePosition)
+    hideAllSlides();
+    
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    //console.log('slidePosition after = ', slidePosition)
+    
+    slides[slidePosition].classList.add("project__carousel-item--visible");
+    slidesTitles[slidePosition].classList.add("product__carousel-item-title--visible");
+}
+
+function moveToPrevSlide() {
+    //console.log('previous button has been clicked')
+    //console.log('slidePosition before = ', slidePosition)
+    hideAllSlides();
+    
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    //console.log('slidePosition after = ', slidePosition)
+    slides[slidePosition].classList.add("project__carousel-item--visible");
+    slidesTitles[slidePosition].classList.add("product__carousel-item-title--visible");
+}
