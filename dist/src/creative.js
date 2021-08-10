9d2557dc2fac2a8b330bd06b88df1139aeabb447
DAVID J RHYNE <davidrhyne@att.net>
2021-08-10 15:20:17 -0500

    enhancing menu and navigation JS, and finalizing styling

--- dist/src/creative.js
+++ dist/src/creative.js
@@ -1,68 +1,81 @@
+/************ menu and navigation variables ************/
 const menu = document.getElementById('nav-toggle')
 const nav = document.getElementById('nav')
-const headerTitle = document.getElementById('header__title')
-// const headerArrow = document.getElementById('header__arrow')
 const hamburger = document.getElementById('hamburger')
 const menuItems = document.querySelectorAll('.menu-item')
-
+const body = document.querySelector('body')
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+let isMenuOpen = false;
 
+/************ carousel variables  ************/
 const slides = document.getElementsByClassName('project__carousel-item')
 const slidesTitles = document.getElementsByClassName('product__carousel-item-title')
 let slidePosition = 0
 const totalSlides = slides.length
 
-let isMenuOpen = false;
 
+/************ menu and navigation section ************/
+
+// handle the clicks on the hamburger and menu items
 function handleMenuClick() {
-    // hide these elements while the menu is open
+
+    // show or hide the menu list 
     nav.classList.toggle('hide')
-    headerTitle.classList.toggle('hide')
-    // headerArrow.classList.toggle('hide')
-    // toggle isMenuOpen to true
+
+    // toggle the value to opposite value
     isMenuOpen = !isMenuOpen;
+    
     // adjust the height of the hamburger
     isMenuOpen === false ? hamburger.style.height = '2px' : hamburger.style.height = '1px';
+    
+    //isMenuOpen ? document.body.classList.remove('nav-open') : document.body.classList.add('nav-open');
 }
 
-// event listeners 
 // for opening and showing menu
 menu.addEventListener('click', handleMenuClick)
 
 // for hiding the menu after an option was selected
 menuItems.forEach(el => el.addEventListener('click', handleMenuClick))
 
-// on load
-nav.classList.add('hide')
+/* watch for clicks outside of the menu and navigation elements
+   if the user clicks outside, then close the menu  */
+body.addEventListener('mouseup', (event) => {
+    const classClicked = event.target.classList
+    const menuItems = ['nav-link', 'nav-toggle', 'hamburger']
+    
+    if(!menuItems.includes(classClicked[0])  ) {
+        document.body.classList.remove('nav-open')
+        nav.classList.add('hide')
+        isMenuOpen = false
+    }
+})
 
+// animate the hambuger into an X shape
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
+// animate the from X shape back to hamburger
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
 })
 
+// on load
+nav.classList.add('hide')
+
+/************ carousel  ************/
+
 // carousel slides + associated slide titles
 document.getElementById('project__carousel-nav-button-next').addEventListener('click', moveToNextSlide);
 document.getElementById('project__carousel-nav-button-previous').addEventListener('click', moveToPrevSlide);
 
-console.log('slidePosition = ', slidePosition)
-console.log('totalSlides = ', totalSlides)
-console.log('slidesTitles.length = ', slidesTitles.length)
-
 function hideAllSlides() {
     for (let slide of slides) {
-        //product__carousel-item-title--visible
-        //product__carousel-item-title--hidden
         slide.classList.remove('project__carousel-item--visible');
         slide.classList.add('project__carousel-item--hidden');
-
-        //slidesTitles.classList.remove('product__carousel-item-title--visible');
-        //slidesTitles.classList.add('product__carousel-item-title--hidden');
     }
 
     for (let title of slidesTitles) {
@@ -72,8 +85,6 @@ function hideAllSlides() {
 }
 
 function moveToNextSlide() {
-    console.log('next button has been clicked')
-    //console.log('slidePosition before = ', slidePosition)
     hideAllSlides();
     
     if (slidePosition === totalSlides - 1) {
@@ -81,15 +92,12 @@ function moveToNextSlide() {
     } else {
         slidePosition++;
     }
-    //console.log('slidePosition after = ', slidePosition)
     
     slides[slidePosition].classList.add("project__carousel-item--visible");
     slidesTitles[slidePosition].classList.add("product__carousel-item-title--visible");
 }
 
 function moveToPrevSlide() {
-    //console.log('previous button has been clicked')
-    //console.log('slidePosition before = ', slidePosition)
     hideAllSlides();
     
     if (slidePosition === 0) {
@@ -97,7 +105,6 @@ function moveToPrevSlide() {
     } else {
         slidePosition--;
     }
-    //console.log('slidePosition after = ', slidePosition)
     slides[slidePosition].classList.add("project__carousel-item--visible");
     slidesTitles[slidePosition].classList.add("product__carousel-item-title--visible");
 }
